f1321abe51a728e71c5991c965a1dde164365585
Jake Zerafa <jake.zerafa86@gmail.com>
2018-09-10 12:36:35 +1000

    Updated Code

---
+++ Learning C#/Assets/AwakeAndStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour {
+    void Awake()
+    {
+        Debug.Log("Awake Called");
+    }
+	void Start () {
+        Debug.Log("Start Called");
+	}
+	
+}
