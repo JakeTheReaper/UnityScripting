090315944eb27fcea3eebf3da79485054f2ece92
Jake Zerafa <jake.zerafa86@gmail.com>
2018-09-10 11:02:50 +1000

    Add New Project

---
+++ Learning C#/Assets/BasicSyntax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSyntax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(transform.position.x);
+
+        if(transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        } else
+        {
+            Debug.Log("I am way too high, I'm FLYING!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Learning C#/Assets/IfStatements.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public float coffeeTemperature = 85.0f;
+    public float hotLimitTemperature = 70.0f;
+    public float coldLimitTemperature = 40.0f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(key: KeyCode.Space))
+        {
+            TemperatureTest();
+        }
+        coffeeTemperature -= Time.deltaTime * .5f;
+	}
+
+    void TemperatureTest()
+    {
+        if(coffeeTemperature > hotLimitTemperature)
+        {
+            Debug.Log("Coffee is too hot.");
+        }
+        else if(coffeeTemperature < coldLimitTemperature)
+        {
+            Debug.Log("Coffee is too cold.");
+        }
+        else
+        {
+            Debug.Log("Coffee is just right.");
+        }
+    }
+}
